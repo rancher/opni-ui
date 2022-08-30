e7e434da95d6074919952a086cbda58e0ceb0bed
Cody Jackson <cody.jackson@suse.com>
2022-08-30 12:44:52 -0700

    Adding logging configuration

---
+++ product/opni/utils/requests/logging.ts
@@ -0,0 +1,77 @@
+import axios from 'axios';
+
+interface ResourceRequirements {
+  limits: { [key: string]: string };
+  requests: { [key: string]: string };
+}
+
+interface Toleration {
+  key: string;
+  operator: string;
+  value: string;
+  taintEffect: string;
+}
+
+interface DataPersistence {
+    Enabled?: boolean;
+    StorageClass?: string;
+}
+
+interface CPUResource {
+    Request: string; // k8s.io.apimachinery.pkg.api.resource.Quantity
+    Limit: string; // k8s.io.apimachinery.pkg.api.resource.Quantity
+}
+
+interface OpensearchNodeDetails {
+    Name: string;
+    Replicas?: number;
+    DiskSize: string; // k8s.io.apimachinery.pkg.api.resource.Quantity
+    MemoryLimit: string; // k8s.io.apimachinery.pkg.api.resource.Quantity
+    CPUResources?: CPUResource;
+    EnableAntiAffinity?: boolean;
+    NodeSelector: { [key: string]: string};
+    Tolerations: Toleration[]; // repeated k8s.io.api.core.v1.Toleration
+    Roles: string[];
+    Persistence?: DataPersistence;
+}
+
+interface DashboardsDetails {
+    Enabled?: boolean;
+    Replicas?: Number;
+    Resources?: ResourceRequirements; // k8s.io.api.core.v1.ResourceRequirements
+}
+
+interface UpgradeAvailableResponse {
+    UpgradePending: boolean;
+}
+
+interface OpensearchCluster {
+    ExternalURL: string;
+    NodePools: OpensearchNodeDetails[];
+    Dashboards?: DashboardsDetails;
+    DataRetention: string;
+}
+
+export async function getStorageClasses(): Promise<string[]> {
+  return (await axios.get('opni-api/LoggingAdmin/logging/storageclasses')).data?.StorageClasses || [];
+}
+
+export async function getLoggingCluster(): Promise<OpensearchCluster> {
+  return (await axios.get('opni-api/LoggingAdmin/logging/cluster')).data;
+}
+
+export async function deleteLoggingCluster(): Promise<any> {
+  await axios.delete('opni-api/LoggingAdmin/logging/cluster');
+}
+
+export async function upsertLoggingCluster(cluster: OpensearchCluster): Promise<any> {
+  return await axios.put('opni-api/LoggingAdmin/logging/cluster', cluster);
+}
+
+export async function isLoggingClusterUpgradeAvailable(): Promise<UpgradeAvailableResponse> {
+  return (await axios.get('opni-api/LoggingAdmin/logging/upgrade/available')).data;
+}
+
+export async function upgradeLoggingCluster() {
+  await axios.post('opni-api/LoggingAdmin/logging/upgrade/do');
+}
