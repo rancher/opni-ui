5b79f65053515948758f0a9f82291aea964cd2c8
codyrancher <cody@rancher.com>
2023-05-01 15:49:14 +0200

    Clearing the entire repo in preperation for the extension migration

--- product/opni/utils/requests/capability.ts
+++
@@ -1,8 +0,0 @@
-import { Capability, CapabilityStatuses } from '~/product/opni/models/Capability';
-import { getClusters } from '~/product/opni/utils/requests/management';
-
-export async function getCapabilities(type: keyof CapabilityStatuses, vue: any): Promise<Capability[]> {
-  const clusters = getClusters(vue);
-
-  return (await clusters).map(c => new Capability(type, c, vue));
-}
